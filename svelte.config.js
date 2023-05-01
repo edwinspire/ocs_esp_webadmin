be17d8ebd083499e61a9d94935980dbb82319520
EDWIN ROBERTO DE LA CRUZ ATAHUALPA <edelacruz@farmaenlace.com>
2023-05-01 07:41:17 -0500

    Corre sobre HTTPS por configuración de VITE en entorno de desarrollo

--- svelte.config.js
+++ svelte.config.js
@@ -3,6 +3,7 @@ import adapter from '@sveltejs/adapter-static';
 import preprocess from 'svelte-preprocess';
 
 
+
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
@@ -11,7 +12,8 @@ const config = {
 
 	kit: {
 		adapter: adapter({ fallback: 'index.html' }),
-		prerender: { entries: [] }
+		paths: { base: '' },
+		prerender: { entries: [] },
 	}
 };
 
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import type { UserConfig } from 'vite';
+import basicSsl from '@vitejs/plugin-basic-ssl'
 
 const config: UserConfig = {
 	logLevel: 'info',
-	plugins: [sveltekit()]
+	server: { https: true },
+	plugins: [sveltekit(), basicSsl()]
 };
 
 export default config;
